ff7db019dadb26bddbb79e2dd5c063a29c8e90c3
Michallesss <mwiemszra@gmail.com>
2022-11-13 21:14:36 +0100

    setup

---
+++ app.js
@@ -0,0 +1,38 @@
+const express=require('express');
+const app=express();
+const dotenv=require('dotenv');
+dotenv.config();
+const path=require('path');
+app.set('view engine','hbs'); //?(ejs)
+app.set('views', path.join(__dirname, 'web/private/views'));
+
+//Public folder
+app.use(express.json());
+app.use('/public', express.static(path.join(__dirname, '/web/public')));
+
+// Host Web Server
+const port=process.env.PORT;
+app.listen(port, (err)=>{
+    if(err) {
+        return console.error('\x1b[31m','Can not start the host:','\x1b[0m',err);
+    }
+    else {
+        return console.log('\x1b[32m','Host listening','\x1b[0m',`(on port ${port})...`,'\x1b[0m');
+    }
+});
+
+// Import Web Routes
+const routes=require('./web/routes')
+app.use('/', routes);
+
+// Error handlers
+// 404
+app.use(function fourOhFourHandler (req, res) {
+    //res.status(404).render('404', {});
+    res.status(404).send('404');
+});
+// 500
+app.use(function fiveHundredHandler (err, req, res, next) {
+    console.error(err)
+    res.status(500).send()
+});
