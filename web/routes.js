af21558bf2e1fdcdd0a244d261dbae1d9132b373
Mikeal <mwiemszra@gmail.com>
2022-11-14 16:15:18 +0100

    routes (sign_up, sign_in, landing)

--- web/routes.js
+++ web/routes.js
@@ -1,6 +1,18 @@
 const router=require('express').Router();
 module.exports=router;
 
+// Landing page
 router.get('/', (req,res)=>{
+    res.render('index', {});
     res.send('Hello World');
+});
+
+// Sign up page
+router.get('/sign_up', (req, res)=>{
+    res.render('sign_up', {});
+});
+
+// Sign in page
+router.get('/sign_in', (req, res)=>{
+    res.render('sign_in', {});
 });
