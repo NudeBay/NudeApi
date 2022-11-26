83d3517b893a5ad8ad653e562448a2b907f75257
Mikeal <mwiemszra@gmail.com>
2022-11-26 12:21:38 +0100

    comment routes without response

--- web/routes.js
+++ web/routes.js
@@ -2,21 +2,21 @@ const router=require('express').Router();
 module.exports=router;
 
 // Dashboard page
-router.get('/', (req,res)=>{
-    return res.render('', {});
-});
+// router.get('/', (req,res)=>{
+//     return res.render('dashboard', {});
+// });
 
 // Chat page
-router.get('/chat', (req,res)=>{
-    return res.render('', {});
-});
+// router.get('/chat', (req,res)=>{
+//     return res.render('chat', {});
+// });
 
 // Sign up page
-router.get('/sign_up', (req, res)=>{
-    return res.render('', {});
-});
+// router.get('/sign_up', (req, res)=>{
+//     return res.render('', {});
+// });
 
 // Sign in page
-router.get('/sign_in', (req, res)=>{
-    return res.render('', {});
-});
+// router.get('/sign_in', (req, res)=>{
+//     return res.render('', {});
+// });
