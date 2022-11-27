5a42486f7bc2e22a5aacc499d6692ba6213d5b96
Mikeal <mwiemszra@gmail.com>
2022-11-28 00:04:20 +0100

    test routes

--- web/routes.js
+++ web/routes.js
@@ -1,7 +1,7 @@
 const router=require('express').Router();
 module.exports=router;
 
-// Dashboard page
+// *Dashboard routes*
 router.get('/', (req,res)=>{
     return res.render('dashboard', {
         pageTitle: 'NudeBay | Dashboard',
@@ -9,21 +9,180 @@ router.get('/', (req,res)=>{
         pageAuthor: 'NudeBay',
         pageKeywords: 'porn, nudes, pussy, free, nudebay, nudebay.com',
         pageType: 'website',
-        pageUrl: 'https://nudebay.com/404',
+        pageUrl: 'https://nudebay.com/',
+        isHome: true,
+        // isSearch: false,
+        // isMessages: false,
+        // isNotifications: false,
+        // isCreate: false,
+        // isTags: false,
+        // isProfile: false,
+        // isSettings: false,
+        body: '<h1>Hello World</h1>',
     });
 });
 
-// Chat page
-// router.get('/chat', (req,res)=>{
-//     return res.render('dashboard', {});
-// });
 
-// Sign up page
+// *Search routes*
+router.get('/search', (req,res)=>{
+    return res.render('dashboard', {
+        pageTitle: 'NudeBay | Search',
+        pageDescription: 'Chaos search.',
+        pageAuthor: 'NudeBay',
+        pageKeywords: 'porn, nudes, pussy, free, nudebay, nudebay.com',
+        pageType: 'website',
+        pageUrl: 'https://nudebay.com/search',
+        // isHome: false,
+        isSearch: true,
+        // isMessages: false,
+        // isNotifications: false,
+        // isCreate: false,
+        // isTags: false,
+        // isProfile: false,
+        // isSettings: false,
+        body: '<h1>Hello Search</h1>',
+    });
+});
+
+
+// *Chat routes*
+router.get('/messages', (req,res)=>{
+    return res.render('dashboard', {
+        pageTitle: 'NudeBay | Inbox',
+        pageDescription: 'Chaos inbox.',
+        pageAuthor: 'NudeBay',
+        pageKeywords: 'porn, nudes, pussy, free, nudebay, nudebay.com',
+        pageType: 'website',
+        pageUrl: 'https://nudebay.com/messages',
+        // isHome: false,
+        // isSearch: false,
+        isMessages: true,
+        // isNotifications: false,
+        // isCreate: false,
+        // isTags: false,
+        // isProfile: false,
+        // isSettings: false,
+        body: '<h1>Hello Messages</h1>',
+    });
+});
+
+
+// *Notifications routes*
+router.get('/notifications', (req,res)=>{
+    return res.render('dashboard', {
+        pageTitle: 'NudeBay | Notifications',
+        pageDescription: 'Chaos notifications.',
+        pageAuthor: 'NudeBay',
+        pageKeywords: 'porn, nudes, pussy, free, nudebay, nudebay.com',
+        pageType: 'website',
+        pageUrl: 'https://nudebay.com/notifications',
+        // isHome: false,
+        // isSearch: false,
+        // isMessages: false,
+        isNotifications: true,
+        // isCreate: false,
+        // isTags: false,
+        // isProfile: false,
+        // isSettings: false,
+        body: '<h1>Hello Notifications</h1>',
+    });
+});
+
+
+// *Create routes*
+router.get('/create', (req,res)=>{
+    return res.render('dashboard', {
+        pageTitle: 'NudeBay | Create',
+        pageDescription: 'Chaos create post.',
+        pageAuthor: 'NudeBay',
+        pageKeywords: 'porn, nudes, pussy, free, nudebay, nudebay.com',
+        pageType: 'website',
+        pageUrl: 'https://nudebay.com/create',
+        // isHome: false,
+        // isSearch: false,
+        // isMessages: false,
+        // isNotifications: false,
+        isCreate: true,
+        // isTags: false,
+        // isProfile: false,
+        // isSettings: false,
+        body: '<h1>Hello Create</h1>',
+    });
+});
+
+
+// *Tags routes*
+router.get('/tags', (req,res)=>{
+    return res.render('dashboard', {
+        pageTitle: 'NudeBay | Tags',
+        pageDescription: 'Chaos post tags.',
+        pageAuthor: 'NudeBay',
+        pageKeywords: 'porn, nudes, pussy, free, nudebay, nudebay.com',
+        pageType: 'website',
+        pageUrl: 'https://nudebay.com/tags',
+        // isHome: false,
+        // isSearch: false,
+        // isMessages: false,
+        // isNotifications: false,
+        // isCreate: false,
+        isTags: true,
+        // isProfile: false,
+        // isSettings: false,
+        body: '<h1>Hello Tags</h1>',
+    });
+});
+
+
+// *Profile routes*
+router.get('/profile', (req,res)=>{
+    return res.render('dashboard', {
+        pageTitle: 'NudeBay | [nameofprofile]',
+        pageDescription: 'Chaos [nameofprofile] profile.',
+        pageAuthor: 'NudeBay',
+        pageKeywords: 'porn, nudes, pussy, free, nudebay, nudebay.com',
+        pageType: 'website',
+        pageUrl: 'https://nudebay.com/profile',
+        // isHome: false,
+        // isSearch: false,
+        // isMessages: false,
+        // isNotifications: false,
+        // isCreate: false,
+        // isTags: false,
+        isProfile: true,
+        // isSettings: false,
+        body: '<h1>Hello [nameofprofile] Profile</h1>',
+    });
+});
+
+// *Settings routes*
+router.get('/settings', (req,res)=>{
+    return res.render('dashboard', {
+        pageTitle: 'NudeBay | Settings',
+        pageDescription: 'Chaos your settings.',
+        pageAuthor: 'NudeBay',
+        pageKeywords: 'porn, nudes, pussy, free, nudebay, nudebay.com',
+        pageType: 'website',
+        pageUrl: 'https://nudebay.com/settings',
+        // isHome: false,
+        // isSearch: false,
+        // isMessages: false,
+        // isNotifications: false,
+        // isCreate: false,
+        // isTags: false,
+        // isProfile: false,
+        isSettings: true,
+        body: '<h1>Hello Settings</h1>',
+    });
+});
+
+
+// *Sign up routes*
 // router.get('/sign_up', (req, res)=>{
 //     return res.render('sign_up', {});
 // });
 
-// Sign in page
+
+// *Sign in routes*
 // router.get('/sign_in', (req, res)=>{
 //     return res.render('sign_up', {});
 // });
