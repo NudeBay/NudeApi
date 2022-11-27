5444343d365cdf8f77706b0b44979ca04a90c7c7
Mikeal <mwiemszra@gmail.com>
2022-11-27 13:00:22 +0100

    add dashboard route

--- web/routes.js
+++ web/routes.js
@@ -2,9 +2,16 @@ const router=require('express').Router();
 module.exports=router;
 
 // Dashboard page
-// router.get('/', (req,res)=>{
-//     return res.render('dashboard', {});
-// });
+router.get('/', (req,res)=>{
+    return res.render('dashboard', {
+        pageTitle: 'NudeBay | Dashboard',
+        pageDescription: 'Chaos Dashboard.',
+        pageAuthor: 'NudeBay',
+        pageKeywords: 'porn, nudes, pussy, free, nudebay, nudebay.com',
+        pageType: 'website',
+        pageUrl: 'https://nudebay.com/404',
+    });
+});
 
 // Chat page
 // router.get('/chat', (req,res)=>{
