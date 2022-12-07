46c4032c153a5f7335dac517b69957d0c7f90438
Mikeal <mwiemszra@gmail.com>
2022-12-07 21:40:14 +0100

    import react

---
+++ web/public/js/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const rootElement=document.querySelector('#root');
+const root=ReactDOM.createRoot(rootElement);
+
+root.render(
+    <>
+        <h1>Hello, world!</h1>
+        <p>Test passed</p>
+    </>
+);
