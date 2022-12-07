0bcb7e22d517b903704f6b2aa5c888d41aa558fd
Mikeal <mwiemszra@gmail.com>
2022-12-07 19:49:29 +0100

    web routes update

--- web/routes.js
+++ web/routes.js
@@ -1,2 +1,30 @@
 const router=require('express').Router();
 module.exports=router;
+
+router.get('/',(req,res)=>{
+    return res.render('index',{
+        pageTitle: 'NudeBay | Dashboard',
+        pageDescription: '',
+        pageAuthor: 'NudeBay',
+        pageKeywords: '',
+        pageType: 'website',
+        pageUrl: 'https://nudebay.com',
+    });
+});
+
+// Error handlers
+// 404
+app.use(function fourOhFourHandler (req, res) {
+    return res.status(404).render('index',{
+        pageTitle: 'NudeBay | 404',
+        pageDescription: 'Cannot find the specific site.',
+        pageAuthor: 'NudeBay',
+        pageKeywords: '404, page not found, nudebay, nudebay.com',
+        pageType: 'website',
+        pageUrl: 'https://nudebay.com/404',
+    });
+});
+// 500
+app.use(function fiveHundredHandler (err, req, res, next) {
+    return res.status(500).send(err)
+});
