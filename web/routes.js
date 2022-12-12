6e5f5cdf5bd607b4e07d429be87ebd63bf9fed64
Mikeal <mwiemszra@gmail.com>
2022-12-12 21:17:26 +0100

    routes for other subsites

--- web/routes.js
+++ web/routes.js
@@ -10,20 +10,87 @@ router.get('/',(req,res)=>{
         pageKeywords: '',
         pageType: 'website',
         pageUrl: 'https://nudebay.com',
+        isHome: true,
     });
 });
 
 // *Search routes
+router.get('/search',(req,res)=>{
+    return res.render('index',{
+        pageTitle: 'NudeBay | Dashboard',
+        pageDescription: '',
+        pageAuthor: 'NudeBay',
+        pageKeywords: '',
+        pageType: 'website',
+        pageUrl: 'https://nudebay.com/search',
+        isSearch: true,
+    });
+});
 
 // *Messages routes
+router.get('/messages',(req,res)=>{
+    return res.render('index',{
+        pageTitle: 'NudeBay | Dashboard',
+        pageDescription: '',
+        pageAuthor: 'NudeBay',
+        pageKeywords: '',
+        pageType: 'website',
+        pageUrl: 'https://nudebay.com/messages',
+        isMessages: true,
+    });
+});
 
 // *Notifications routes
+router.get('/notifications',(req,res)=>{
+    return res.render('index',{
+        pageTitle: 'NudeBay | Dashboard',
+        pageDescription: '',
+        pageAuthor: 'NudeBay',
+        pageKeywords: '',
+        pageType: 'website',
+        pageUrl: 'https://nudebay.com/notifications',
+        isNotifications: true,
+    });
+});
 
 // *Create routes
+router.get('/create',(req,res)=>{
+    return res.render('index',{
+        pageTitle: 'NudeBay | Dashboard',
+        pageDescription: '',
+        pageAuthor: 'NudeBay',
+        pageKeywords: '',
+        pageType: 'website',
+        pageUrl: 'https://nudebay.com/create',
+        isCreate: true,
+    });
+});
 
 // *Profile routes
+router.get('/profile',(req,res)=>{
+    return res.render('index',{
+        pageTitle: 'NudeBay | Dashboard',
+        pageDescription: '',
+        pageAuthor: 'NudeBay',
+        pageKeywords: '',
+        pageType: 'website',
+        pageUrl: 'https://nudebay.com/profile',
+        isProfile: true,
+    });
+});
 
 // *Settings routes
+router.get('/settings',(req,res)=>{
+    return res.render('index',{
+        pageTitle: 'NudeBay | Dashboard',
+        pageDescription: '',
+        pageAuthor: 'NudeBay',
+        pageKeywords: '',
+        pageType: 'website',
+        pageUrl: 'https://nudebay.com/settings',
+        isSettings: true,
+    });
+});
 
 // !Error handlers
 // *404
