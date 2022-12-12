c52530bfb9a02e9f3931e8116ac790a9e335551c
Mikeal <mwiemszra@gmail.com>
2022-12-12 16:26:17 +0100

    App component

---
+++ web/public/js/components/App.js
@@ -0,0 +1,5 @@
+export default function App() {
+    return (
+        <div className="home-container">App is launched</div>
+    );
+};
--- web/public/js/index.js
+++ web/public/js/index.js
@@ -1,11 +1,7 @@
 'use strict';
-
 const rootElement=document.querySelector('#root');
 const root=ReactDOM.createRoot(rootElement);
 
 root.render(
-    <>
-        <h1>Hello, world!</h1>
-        <p>Test passed</p>
-    </>
+    <App />
 );
