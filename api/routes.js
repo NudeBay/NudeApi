a1a40d1e82577dd07a147ab0efec220e6043d03e
Mikeal <mwiemszra@gmail.com>
2022-12-17 23:32:31 +0100

    handle api errors

--- api/routes.js
+++ api/routes.js
@@ -1,2 +1,14 @@
 const router=require('express').Router();
 module.exports=router;
+
+// ...other routes
+
+// !Error handlers
+// *404
+router.use(function fourOhFourHandler (req, res) {
+    return res.status(404).send('404 Not Found');
+});
+// *500
+router.use(function fiveHundredHandler (err, req, res, next) {
+    return res.status(500).send('500 Internal Server Error');
+});
