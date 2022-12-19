1f38002490d380ed49672297bcfa14b860fba30b
Mikeal <mwiemszra@gmail.com>
2022-12-19 12:50:29 +0100

    working nav-bar with custom pushstate function

--- web/public/react/components/App.js
+++ web/public/react/components/App.js
@@ -1,39 +1,113 @@
 const App = () => {
+    const handleMenuClick = (state=null, unused=null, url=null) => {
+        const currentUrl = window.location.pathname;
+        if((state!==null || unused!==null || url!==null) && (currentUrl!==url)) {
+            history.pushState(state, unused, url);
+        }
+        switch(url) {
+            case '/':
+                document.querySelectorAll('.menu-item > img').forEach((img) => img.src = "../../public/svgs/line/" + img.alt + ".svg");
+                document.querySelector('#menu-home > img').src = "../../public/svgs/solid/home.svg";
+                break;
+            case '/search':
+                document.querySelectorAll('.menu-item > img').forEach((img) => img.src = "../../public/svgs/line/" + img.alt + ".svg");
+                document.querySelector('#menu-search > img').src = "../../public/svgs/solid/search.svg";
+                break;
+            case '/messages':
+                document.querySelectorAll('.menu-item > img').forEach((img) => img.src = "../../public/svgs/line/" + img.alt + ".svg");
+                document.querySelector('#menu-messages > img').src = "../../public/svgs/solid/chat.svg";
+                break;
+            case '/notifications':
+                document.querySelectorAll('.menu-item > img').forEach((img) => img.src = "../../public/svgs/line/" + img.alt + ".svg");
+                document.querySelector('#menu-notifications > img').src = "../../public/svgs/solid/collection.svg";
+                break;
+            case '/create':
+                document.querySelectorAll('.menu-item > img').forEach((img) => img.src = "../../public/svgs/line/" + img.alt + ".svg");
+                document.querySelector('#menu-create > img').src = "../../public/svgs/solid/plus.svg";
+                break;
+            case '/profile':
+                document.querySelectorAll('.menu-item > img').forEach((img) => img.src = "../../public/svgs/line/" + img.alt + ".svg");
+                document.querySelector('#menu-profile > img').src = "../../public/svgs/solid/user.svg";
+                break;
+            case '/settings':
+                document.querySelectorAll('.menu-item > img').forEach((img) => img.src = "../../public/svgs/line/" + img.alt + ".svg");
+                document.querySelector('#menu-settings > img').src = "../../public/svgs/solid/adjustments.svg";
+                break;
+            default:
+                document.querySelectorAll('.menu-item > img').forEach((img) => img.src = "../../public/svgs/line/" + img.alt + ".svg");
+                break;
+        }
+    }
+
+    let 
+    homeIcon = "../../public/svgs/line/home.svg",
+    searchIcon = "../../public/svgs/line/search.svg",
+    messagesIcon = "../../public/svgs/line/chat.svg",
+    notificationsIcon = "../../public/svgs/line/collection.svg",
+    createIcon = "../../public/svgs/line/plus.svg",
+    profileIcon = "../../public/svgs/line/user.svg",
+    settingsIcon = "../../public/svgs/line/adjustments.svg";
+    switch(window.location.pathname) {
+        case '/':
+            homeIcon = "../../public/svgs/solid/home.svg";
+            break;
+        case '/search':
+            searchIcon = "../../public/svgs/solid/search.svg";
+            break;
+        case '/messages':
+            messagesIcon = "../../public/svgs/solid/chat.svg";
+            break;
+        case '/notifications':
+            notificationsIcon = "../../public/svgs/solid/collection.svg";
+            break;
+        case '/create':
+            createIcon = "../../public/svgs/solid/plus.svg";
+            break;
+        case '/profile':
+            profileIcon = "../../public/svgs/solid/user.svg";
+            break;
+        case '/settings':
+            settingsIcon = "../../public/svgs/solid/adjustments.svg";
+            break;
+        default:
+            break;
+    }
+    
     return (
         <>
             <div className="left-panel">
                 <div className="logo">
-                    <a href="/"> 
+                    <a href="/" onClick={(e) => handleMenuClick(null,'','/')}> 
                         <img src={"../../public/images/logo.png"} draggable={false} alt={"logo"}/>
                     </a>
                 </div>
                 <ul className="menu">
-                    <li className="menu-item" id="menu-home" onClick={(e) => history.pushState(null,'','/')}>
-                        <img src={"../../public/svgs/line/home.svg"} draggable={false} alt={"Home"} className="icon"/>
+                    <li className="menu-item" id="menu-home" onClick={(e) => handleMenuClick(null,'','/')}>
+                        <img src={homeIcon} draggable={false} alt={"home"} className="icon"/>
                         <span className="text">Home</span>
                     </li>
-                    <li className="menu-item" id="menu-search" onClick={(e) => history.pushState(null,'','/search')}>
-                        <img src={"../../public/svgs/line/search.svg"} draggable={false} alt={"Search"} className="icon"/>
+                    <li className="menu-item" id="menu-search" onClick={(e) => handleMenuClick(null,'','/search')}>
+                        <img src={searchIcon} draggable={false} alt={"search"} className="icon"/>
                         <span className="text">Search</span>
                     </li>
-                    <li className="menu-item" id="menu-messages" onClick={(e) => history.pushState(null,'','/messages')}>
-                        <img src={"../../public/svgs/line/chat.svg"} draggable={false} alt={"Messages"} className="icon"/>
+                    <li className="menu-item" id="menu-messages" onClick={(e) => handleMenuClick(null,'','/messages')}>
+                        <img src={messagesIcon} draggable={false} alt={"chat"} className="icon"/>
                         <span className="text">Messages</span>
                     </li>
-                    <li className="menu-item" id="menu-notifications" onClick={(e) => history.pushState(null,'','/notifications')}>
-                        <img src={"../../public/svgs/line/collection.svg"} draggable={false} alt={"Notifications"} className="icon"/>
+                    <li className="menu-item" id="menu-notifications" onClick={(e) => handleMenuClick(null,'','/notifications')}>
+                        <img src={notificationsIcon} draggable={false} alt={"collection"} className="icon"/>
                         <span className="text">Notifications</span>
                     </li>
-                    <li className="menu-item" id="menu-create" onClick={(e) => history.pushState(null,'','/create')}>
-                        <img src={"../../public/svgs/line/plus.svg"} draggable={false} alt={"Create"} className="icon"/>
+                    <li className="menu-item" id="menu-create" onClick={(e) => handleMenuClick(null,'','/create')}>
+                        <img src={createIcon} draggable={false} alt={"plus"} className="icon"/>
                         <span className="text">Create</span>
                     </li>
-                    <li className="menu-item" id="menu-profile" onClick={(e) => history.pushState(null,'','/profile')}>
-                        <img src={"../../public/svgs/line/user.svg"} draggable={false} alt={"Profile"} className="icon"/>
+                    <li className="menu-item" id="menu-profile" onClick={(e) => handleMenuClick(null,'','/profile')}>
+                        <img src={profileIcon} draggable={false} alt={"user"} className="icon"/>
                         <span className="text">Profile</span>
                     </li>
-                    <li className="menu-item" id="menu-settings" onClick={(e) => history.pushState(null,'','/settings')}>
-                        <img src={"../../public/svgs/line/adjustments.svg"} draggable={false} alt={"Settings"} className="icon"/>
+                    <li className="menu-item" id="menu-settings" onClick={(e) => handleMenuClick(null,'','/settings')}>
+                        <img src={settingsIcon} draggable={false} alt={"adjustments"} className="icon"/>
                         <span className="text">Settings</span>
                     </li>
                 </ul>
