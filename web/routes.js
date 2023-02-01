94d4de465d420e97846085b842d1e6c1768b2c01
Mikeal <mwiemszra@gmail.com>
2023-02-01 23:52:33 +0100

    remove console logs on errors

--- web/routes.js
+++ web/routes.js
@@ -6,15 +6,13 @@ router.get('*',(req,res)=>{
     return res.sendFile('./public/html/index.html',  { root: __dirname });
 });
 
+
 // !Error handlers
 // *404
 router.use(function fourOhFourHandler (req, res) {
-    console.error('\x1b[31m','404 error','\x1b[0m');
     res.status(404).send('Cannot get the requested page. We work on it. Try again later.');
-    // error 404 should not exist in production bc of get * route
 });
 // *500
 router.use(function fiveHundredHandler (err, req, res, next) {
-    console.error('\x1b[31m','500 error','\x1b[0m',err);
     res.status(500).send('The server has encountered a situation it does not know how to handle. We work on it. Try again later.');
 });
