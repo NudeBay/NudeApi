4185bb9fad78b6af28133338103d2aa871b808e3
Mikeal <mwiemszra@gmail.com>
2023-02-24 19:47:07 +0100

    🔒️ responses didn't return for whole function

--- api/middlewares/verify.js
+++ api/middlewares/verify.js
@@ -3,32 +3,37 @@ const jwt=require('jsonwebtoken');
 const mongoose=require('mongoose'); // ? and that one
 const User=require('../models/users');
 
-module.exports=(req, res, next) => {
-    const token=req.header('auth-token'); // Get token
-    if(!token) return res.status(401).send('Access Denied'); // Check if token exists
-    jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => { // Verify token
-        if(err) return res.status(400).send('Invalid Token');
-        else {
-            // Check if user exists (and not deleted)
-            const [ isValid, foundUser ]=User.findOne({"_id": decoded._id, "delete.isDeleted":false}).then((foundUser) => {
-                if(foundUser) return [ true, foundUser ];
-                else return [ false, null ];
-            }).catch((err) => {
-                return [ false, null ];
-            });
-            if(!isValid) return res.status(500).send('500 Internal Server Error');
-            
-            // Check if user is banned
-            const foundBan=foundUser.bans.find(ban => ban.banExpirationDate>new Date());
-            if(foundBan) return res.status(403).send(`You are banned until ${foundBan.banExpirationDate} for "${foundBan.banReason}"`);
+module.exports=async (req, res, next) => {
+    // Get token
+    const token=await req.header('auth-token'); 
+    if(!token) return res.status(401).send('Access Denied'); 
 
-            // Check if user's device is in device list
-            const foundDevice=foundUser.devices.find(device => device.ip===req.socket.remoteAddress ?? req.ip);
-            if(!foundDevice) return res.status(401).send('This IP is unauthorized');
-
-            // Pass and return found user object
-            res.locals.user=foundUser; // ? should I also return decoded token values
-            return next();
-        }
+    // Check if token exists and is valid
+    const [ isValidToken, decoded ]=await jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+        if(err) return [ false, null ];
+        else return [ true, decoded ];
     });
+    if(!isValidToken) return res.status(400).send('Invalid Token');
+
+    // Check if user exists (and not deleted)
+    const [ isFindError, foundUser ]=await User.findOne({"_id":decoded._id, "delete.isDeleted":false}).then((user) => {
+        if(user) return [ false, user ];
+        else return [ false, null ];
+    }).catch((err) => {
+        return [ true, null ];
+    });
+    if(isFindError) return res.status(500).send('500 Internal Server Error');
+    if(!foundUser) return res.status(401).send('This tokenID is unauthorized');
+    
+    // Check if user is banned
+    const foundBan=await foundUser.bans.find(ban => ban.banExpirationDate>new Date());
+    if(foundBan) return res.status(403).send(`You are banned until ${foundBan.banExpirationDate} for "${foundBan.banReason}"`);
+
+    // Check if user's device is in device list
+    const foundDevice=await foundUser.devices.find(device => device.ip===req.socket.remoteAddress);
+    if(!foundDevice) return res.status(401).send('This IP is unauthorized');
+
+    // Pass and return found user object
+    res.locals.user=foundUser; // ? should I also return decoded token values
+    return next();
 };
