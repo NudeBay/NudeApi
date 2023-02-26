f5997a5d52a12578910ae06605c68a25894137da
Mikeal <mwiemszra@gmail.com>
2023-02-26 22:35:32 +0100

    🚧 add todos for search

--- api/routes.js
+++ api/routes.js
@@ -30,7 +30,7 @@ mongoose.connect(uri, {
 
 
 // *Home routes
-router.get('/home', verify, async (req, res) => { // !Remember to not display posts from blocked users and with compartment
+router.get('/home', verify, async (req, res) => {   // !Remember to not display posts from blocked users and with compartment
     // Get posts (from followed users)
 
     // Get posts (from friends)
@@ -46,7 +46,35 @@ router.get('/home', verify, async (req, res) => { // !Remember to not display po
 
 
 // *Search routes
-// ...
+router.get('/search/:text', verify, async (req, res) => {   // !Remember to not display posts from blocked users and with compartment
+    // Get users (by name)
+
+    // Get users (by email)
+
+    // Get users (by description)
+
+    // Get posts (by text)
+
+    // Get posts (by tags)
+
+    // Get posts (by users)
+
+    // Mix results
+
+    // Send results (as list of objects)
+});
+
+router.get('/search/#:tag', verify, async (req, res) => {   // !Remember to not display posts from blocked users and with compartment
+    // Get posts (by tag)
+
+    // Send results (as list of objects)
+});
+
+router.get('/search/@:user', verify, async (req, res) => {  // !Remember to not display posts from blocked users and with compartment
+    // Get posts (by user)
+
+    // Send results (as list of objects)
+});
 
 
 // *Messages routes
