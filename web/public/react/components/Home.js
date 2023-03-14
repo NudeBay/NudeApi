c6258ca363a32a834a74cded00504069db251b1a
Mikeal <mwiemszra@gmail.com>
2023-03-14 22:37:49 +0100

    🚧 player component (view counter, time formatter)

--- web/public/react/components/Home.js
+++ web/public/react/components/Home.js
@@ -7,62 +7,23 @@ function Home() {
 
     return (
         <div className="home-container">
+            <Post type="1" content="2" tags="3" author="4" date={new Date().toString()} likes="6" views={2137} />
             <div className="player">
-                <div className="top-bar" onclick="window.location='/profile'">
-                    <img src="https://i.pinimg.com/736x/65/59/37/6559374e52a921ad76785d2175db21c2.jpg" alt="[profil_name]" title="[profil_name]" className="icon" draggable="false" />
-                    <span className="text" title="see profile">Zuzaa Olechowska</span>
-                    <div className="badges">
-                        <img src="../../public/svgs/line/badge-check.svg" alt="[badge_name]" title="[badge_name]" className="badge" draggable="false" />
-                        <img src="../../public/svgs/line/badge-check.svg" alt="[badge_name]" title="[badge_name]" className="badge" draggable="false" />
-                    </div>
-                    <ul className="info">
-                        <li className="info-item"><img src="../../public/svgs/line/play.svg" alt="" title draggable="false" />13k</li>
-                        <li className="info-item"><img src="../../public/svgs/line/calendar.svg" alt="" title draggable="false" />Dec 4, 2022 at 12:59 AM</li>
-                    </ul>
-                </div>
-                <div className="content">
-                    <img src="https://i.pinimg.com/564x/3c/20/5a/3c205a81d5ed0ddab624d6b58b40d91e.jpg" alt="" title />
-                </div>
-                <div className="bot-bar">
-                    <ul className="menu">
-                        <li className="menu-item">
-                            <img src="../../public/svgs/line/heart.svg" alt="like" title="like" draggable="false" />
-                        </li>
-                        <li className="menu-item">
-                            <img src="../../public/svgs/line/eye.svg" alt="see more" title="see more" draggable="false" />
-                        </li>
-                        <li className="menu-item">
-                            <img src="../../public/svgs/line/chat.svg" alt="comments" title="comments" draggable="false" />
-                        </li>
-                        <li className="menu-item">
-                            <img src="../../public/svgs/line/exclamation-circle.svg" alt="report" title="report" draggable="false" />
-                        </li>
-                        <li className="menu-item">
-                            <img src="../../public/svgs/line/bookmark.svg" alt="save" title="save" draggable="false" />
-                        </li>
-                    </ul>
-                    <ul className="tag-list">
-                        <li className="tag" onclick="window.location='/'">dupa</li>
-                        <li className="tag" onclick="window.location='/'">kupa</li>
-                    </ul>
-                </div>
-            </div>
-            <div className="player">
-                <div className="top-bar" onclick="window.location='/profile'">
+                <div className="top-bar" onClick={(e) => window.location='pornhub.com/gay'}>
                     <img src="https://i.pinimg.com/564x/97/e9/4a/97e94aad4e9550ffde571016105b34ac.jpg" alt="[profil_name]" title="[profil_name]" className="icon" draggable="false" />
                     <span className="text" title="see profile">Użytkownik</span>
                     <div className="badges">
                         <img src="../../public/svgs/line/ban.svg" alt="ban" title="ban" className="badge" draggable="false" />
                     </div>
                     <ul className="info">
-                        <li className="info-item"><img src="../../public/svgs/line/play.svg" alt="" title draggable="false" />1,3m</li>
-                        <li className="info-item"><img src="../../public/svgs/line/calendar.svg" alt="" title draggable="false" />Dec 12, 2022 at 10:23 PM</li>
+                        <li className="info-item"><img src="../../public/svgs/line/play.svg" alt="" title="" draggable="false" />1,3m</li>
+                        <li className="info-item"><img src="../../public/svgs/line/calendar.svg" alt="" title="" draggable="false" />Dec 12, 2022 at 10:23 PM</li>
                     </ul>
                 </div>
                 <div className="content">
-                    <video poster="https://thumbs4.redgifs.com/DeficientSleepyAmurminnow-mobile.jpg?expires=1670338800&signature=cd7538cc43e130caab6bae577edd90c77c85a297e5954a0eb8f3bc64107794fe&for=83.22.52.122" preload="metadata" playsInline autoPlay muted loop controls title>
-                        <source src="https://thumbs44.redgifs.com/WavyCyanMolly-mobile.mp4?expires=1677530400&signature=v2:9055a750eaf8b60630366470f20d8298a04708986181042390e31d2a132523dc&for=83.22.50.85&hash=6163438793" type="video/mp4" />
-                        <source src="https://thumbs44.redgifs.com/WavyCyanMolly-mobile.mp4?expires=1677530400&signature=v2:9055a750eaf8b60630366470f20d8298a04708986181042390e31d2a132523dc&for=83.22.50.85&hash=6163438793" type="video/ogg" />
+                    <video poster="" preload="metadata" playsInline autoPlay muted loop controls title="">
+                        <source src="" type="video/mp4" />
+                        <source src="" type="video/ogg" />
                         Your browser does not support the videos.
                     </video>
                 </div>
@@ -85,13 +46,13 @@ function Home() {
                         </li>
                     </ul>
                     <ul className="tag-list">
-                        <li className="tag" onclick="window.location='/'">fajfus</li>
-                        <li className="tag" onclick="window.location='/'">dsdadas</li>
+                        <li className="tag" onClick={(e) => window.location='pornhub.com/gay'}>fajfus</li>
+                        <li className="tag" onClick={(e) => window.location='pornhub.com/gay'}>dsdadas</li>
                     </ul>
                 </div>
             </div>
             <div className="player">
-                <div className="top-bar" onclick="window.location='/profile'">
+                <div className="top-bar" onClick={(e) => window.location='pornhub.com/gay'}>
                     <img src="https://i.pinimg.com/564x/f3/73/b7/f373b74b6bf8cd6d1422d03c01990e28.jpg" alt="[profil_name]" title="[profil_name]" className="icon" draggable="false" />
                     <span className="text" title="see profile">Zuzaa Olechowska</span>
                     <div className="badges">
@@ -99,8 +60,8 @@ function Home() {
                         <img src="../../public/svgs/solid/badge-check.svg" alt="[badge_name]" title="[badge_name]" className="badge" draggable="false" />
                     </div>
                     <ul className="info">
-                        <li className="info-item"><img src="../../public/svgs/line/play.svg" alt="" title draggable="false" />972</li>
-                        <li className="info-item"><img src="../../public/svgs/line/calendar.svg" alt="" title draggable="false" />Nov 6, 2022 at 4:49 AM</li>
+                        <li className="info-item"><img src="../../public/svgs/line/play.svg" alt="" title="" draggable="false" />972</li>
+                        <li className="info-item"><img src="../../public/svgs/line/calendar.svg" alt="" title="" draggable="false" />Nov 6, 2022 at 4:49 AM</li>
                     </ul>
                 </div>
                 <div className="content">
@@ -129,7 +90,7 @@ function Home() {
                 </div>
             </div>
             <div className="player">
-                <div className="top-bar" onclick="window.location='/profile'">
+                <div className="top-bar" onClick={(e) => window.location='pornhub.com/gay'}>
                     <img src="https://i.pinimg.com/564x/90/02/26/9002260fc9035fbd78fc4c567c6982c1.jpg" alt="[profil_name]" title="[profil_name]" className="icon" draggable="false" />
                     <span className="text" title="see profile">Agata Max Bednarek</span>
                     <div className="badges">
@@ -137,12 +98,12 @@ function Home() {
                         <img src="../../public/svgs/solid/check-circle.svg" alt="[badge_name]" title="[badge_name]" className="badge" draggable="false" />
                     </div>
                     <ul className="info">
-                        <li className="info-item"><img src="../../public/svgs/line/play.svg" alt="" title draggable="false" />71</li>
-                        <li className="info-item"><img src="../../public/svgs/line/calendar.svg" alt="" title draggable="false" />Nov 19, 2021 at 3:36 AM</li>
+                        <li className="info-item"><img src="../../public/svgs/line/play.svg" alt="" title="" draggable="false" />71</li>
+                        <li className="info-item"><img src="../../public/svgs/line/calendar.svg" alt="" title="" draggable="false" />Nov 19, 2021 at 3:36 AM</li>
                     </ul>
                 </div>
                 <div className="content">
-                    <img src="https://i.pinimg.com/564x/31/47/ab/3147ab80acf3b17edd56bba0a2052bf4.jpg" alt="" title />
+                    <img src="https://i.pinimg.com/564x/31/47/ab/3147ab80acf3b17edd56bba0a2052bf4.jpg" alt="" title="" />
                 </div>
                 <div className="bot-bar">
                     <ul className="menu">
@@ -163,10 +124,106 @@ function Home() {
                         </li>
                     </ul>
                     <ul className="tag-list">
-                        <li className="tag" onclick="window.location='/'">dupa</li>
-                        <li className="tag" onclick="window.location='/'">kupa</li>
+                        <li className="tag" onClick={(e) => window.location='pornhub.com/gay'}>dupa</li>
+                        <li className="tag" onClick={(e) => window.location='pornhub.com/gay'}>kupa</li>
                     </ul>
                 </div>
+            </div>
+        </div>
+    );
+}
+
+function Post({ type, content, tags, author: {nick, picture, bages }, date, likes, views }) { // ! include ban to bages
+    // Content format
+
+    // Tags format
+
+    // Author format
+
+    // Date format: mm dd, yyyy at hh:mm
+    date=new Date(date);
+    const month=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+    let newDate=month[date.getMonth()]+' '+date.getDate()+', '+date.getFullYear();//+' at '+date.getHours()+':'+date.getMinutes();
+
+    // Views format: 1.2k, 1.2m, 1.2b
+    let newViews=views;
+    if (views>=1000) {
+        newViews=views/1000;
+        if (views>=1000000) {
+            newViews=views/1000000;
+            if (views>=1000000000) {
+                newViews=views/1000000000;
+                newViews=newViews.toFixed(1)+'b';
+            } else {
+                newViews=newViews.toFixed(1)+'m';
+            }
+        } else {
+            newViews=newViews.toFixed(1)+'k';
+        }
+    }
+
+    // Likes format: 1.2k, 1.2m, 1.2b
+    let newLikes=likes;
+    if (likes>=1000) {
+        newLikes=likes/1000;
+        if (likes>=1000000) {
+            newLikes=likes/1000000;
+            if (likes>=1000000000) {
+                newLikes=likes/1000000000;
+                newLikes=newLikes.toFixed(1)+'b';
+            } else {
+                newLikes=newLikes.toFixed(1)+'m';
+            }
+        } else {
+            newLikes=newLikes.toFixed(1)+'k';
+        }
+    }
+
+    return (
+        <div className="player">
+            <div className="top-bar" onClick={(e) => window.location='pornhub.com/gay'}>
+                <img src="https://i.pinimg.com/736x/65/59/37/6559374e52a921ad76785d2175db21c2.jpg" alt="[profil_name]" title="[profil_name]" className="icon" draggable="false" />
+                <span className="text" title="see profile">Zuzaa Olechowska</span>
+                <div className="badges">
+                    <img src="../../public/svgs/line/badge-check.svg" alt="[badge_name]" title="[badge_name]" className="badge" draggable="false" />
+                    <img src="../../public/svgs/line/badge-check.svg" alt="[badge_name]" title="[badge_name]" className="badge" draggable="false" />
+                </div>
+                <ul className="info">
+                    <li className="info-item">
+                        <img src="../../public/svgs/line/play.svg" alt="" title="" draggable="false" />
+                        {newViews}
+                    </li>
+                    <li className="info-item">
+                        <img src="../../public/svgs/line/calendar.svg" alt="" title="" draggable="false" />
+                        {newDate}
+                    </li>
+                </ul>
+            </div>
+            <div className="content">
+                <img src="https://i.pinimg.com/564x/3c/20/5a/3c205a81d5ed0ddab624d6b58b40d91e.jpg" alt="" title="" />
+            </div>
+            <div className="bot-bar">
+                <ul className="menu">
+                    <li className="menu-item">
+                        <img src="../../public/svgs/line/heart.svg" alt="like" title="like" draggable="false" />
+                    </li>
+                    <li className="menu-item">
+                        <img src="../../public/svgs/line/eye.svg" alt="see more" title="see more" draggable="false" />
+                    </li>
+                    <li className="menu-item">
+                        <img src="../../public/svgs/line/chat.svg" alt="comments" title="comments" draggable="false" />
+                    </li>
+                    <li className="menu-item">
+                        <img src="../../public/svgs/line/exclamation-circle.svg" alt="report" title="report" draggable="false" />
+                    </li>
+                    <li className="menu-item">
+                        <img src="../../public/svgs/line/bookmark.svg" alt="save" title="save" draggable="false" />
+                    </li>
+                </ul>
+                <ul className="tag-list">
+                    <li className="tag" onClick={(e) => window.location='pornhub.com/gay'}>dupa</li>
+                    <li className="tag" onClick={(e) => window.location='pornhub.com/gay'}>kupa</li>
+                </ul>
             </div>
         </div>
     );
