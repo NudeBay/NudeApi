b4bafb3ed841a7c61586261e2fb06e12e4b59ff5
Mikeal <mwiemszra@gmail.com>
2023-03-16 20:25:20 +0100

    🚧 add component content (still to update)

--- web/public/react/components/Home.js
+++ web/public/react/components/Home.js
@@ -7,138 +7,30 @@ function Home() {
 
     return (
         <div className="home-container">
-            <Post type="1" content="2" tags="3" author="4" date={new Date().toString()} likes="6" views={2137} />
-            <div className="player">
-                <div className="top-bar" onClick={(e) => window.location='pornhub.com/gay'}>
-                    <img src="https://i.pinimg.com/564x/97/e9/4a/97e94aad4e9550ffde571016105b34ac.jpg" alt="[profil_name]" title="[profil_name]" className="icon" draggable="false" />
-                    <span className="text" title="see profile">Użytkownik</span>
-                    <div className="badges">
-                        <img src="../../public/svgs/line/ban.svg" alt="ban" title="ban" className="badge" draggable="false" />
-                    </div>
-                    <ul className="info">
-                        <li className="info-item"><img src="../../public/svgs/line/play.svg" alt="" title="" draggable="false" />1,3m</li>
-                        <li className="info-item"><img src="../../public/svgs/line/calendar.svg" alt="" title="" draggable="false" />Dec 12, 2022 at 10:23 PM</li>
-                    </ul>
-                </div>
-                <div className="content">
-                    <video poster="" preload="metadata" playsInline autoPlay muted loop controls title="">
-                        <source src="" type="video/mp4" />
-                        <source src="" type="video/ogg" />
-                        Your browser does not support the videos.
-                    </video>
-                </div>
-                <div className="bot-bar">
-                    <ul className="menu">
-                        <li className="menu-item">
-                            <img src="../../public/svgs/solid/heart.svg" alt="like" title="like" draggable="false" />
-                        </li>
-                        <li className="menu-item">
-                            <img src="../../public/svgs/line/eye.svg" alt="see more" title="see more" draggable="false" />
-                        </li>
-                        <li className="menu-item">
-                            <img src="../../public//svgs/line/chat.svg" alt="comments" title="comments" draggable="false" />
-                        </li>
-                        <li className="menu-item">
-                            <img src="../../public/svgs/line/exclamation-circle.svg" alt="report" title="report" draggable="false" />
-                        </li>
-                        <li className="menu-item">
-                            <img src="../../public/svgs/solid/bookmark.svg" alt="save" title="save" draggable="false" />
-                        </li>
-                    </ul>
-                    <ul className="tag-list">
-                        <li className="tag" onClick={(e) => window.location='pornhub.com/gay'}>fajfus</li>
-                        <li className="tag" onClick={(e) => window.location='pornhub.com/gay'}>dsdadas</li>
-                    </ul>
-                </div>
-            </div>
-            <div className="player">
-                <div className="top-bar" onClick={(e) => window.location='pornhub.com/gay'}>
-                    <img src="https://i.pinimg.com/564x/f3/73/b7/f373b74b6bf8cd6d1422d03c01990e28.jpg" alt="[profil_name]" title="[profil_name]" className="icon" draggable="false" />
-                    <span className="text" title="see profile">Zuzaa Olechowska</span>
-                    <div className="badges">
-                        <img src="../../public/svgs/line/badge-check.svg" alt="[badge_name]" title="[badge_name]" className="badge" draggable="false" />
-                        <img src="../../public/svgs/solid/badge-check.svg" alt="[badge_name]" title="[badge_name]" className="badge" draggable="false" />
-                    </div>
-                    <ul className="info">
-                        <li className="info-item"><img src="../../public/svgs/line/play.svg" alt="" title="" draggable="false" />972</li>
-                        <li className="info-item"><img src="../../public/svgs/line/calendar.svg" alt="" title="" draggable="false" />Nov 6, 2022 at 4:49 AM</li>
-                    </ul>
-                </div>
-                <div className="content">
-                    <span>
-                        Slub za minute!<br /> Zapraszamy na discorda Mokrypapież.sex <br /> KOCHAM PIWO JA CHCE WYPIĆ PIWO PIWO TO PALIWO WYRUCHAĆ KURWEKE WSADZIĆ CHUJA GEMBE
-                    </span>
-                </div>
-                <div className="bot-bar">
-                    <ul className="menu">
-                        <li className="menu-item">
-                            <img src="../../public/svgs/line/heart.svg" alt="like" title="like" draggable="false" />
-                        </li>
-                        <li className="menu-item">
-                            <img src="../../public/svgs/line/eye.svg" alt="see more" title="see more" draggable="false" />
-                        </li>
-                        <li className="menu-item">
-                            <img src="../../public/svgs/line/chat.svg" alt="comments" title="comments" draggable="false" />
-                        </li>
-                        <li className="menu-item">
-                            <img src="../../public/svgs/line/exclamation-circle.svg" alt="report" title="report" draggable="false" />
-                        </li>
-                        <li className="menu-item">
-                            <img src="../../public/svgs/line/bookmark.svg" alt="save" title="save" draggable="false" />
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div className="player">
-                <div className="top-bar" onClick={(e) => window.location='pornhub.com/gay'}>
-                    <img src="https://i.pinimg.com/564x/90/02/26/9002260fc9035fbd78fc4c567c6982c1.jpg" alt="[profil_name]" title="[profil_name]" className="icon" draggable="false" />
-                    <span className="text" title="see profile">Agata Max Bednarek</span>
-                    <div className="badges">
-                        <img src="../../public/svgs/line/check-circle.svg" alt="[badge_name]" title="[badge_name]" className="badge" draggable="false" />
-                        <img src="../../public/svgs/solid/check-circle.svg" alt="[badge_name]" title="[badge_name]" className="badge" draggable="false" />
-                    </div>
-                    <ul className="info">
-                        <li className="info-item"><img src="../../public/svgs/line/play.svg" alt="" title="" draggable="false" />71</li>
-                        <li className="info-item"><img src="../../public/svgs/line/calendar.svg" alt="" title="" draggable="false" />Nov 19, 2021 at 3:36 AM</li>
-                    </ul>
-                </div>
-                <div className="content">
-                    <img src="https://i.pinimg.com/564x/31/47/ab/3147ab80acf3b17edd56bba0a2052bf4.jpg" alt="" title="" />
-                </div>
-                <div className="bot-bar">
-                    <ul className="menu">
-                        <li className="menu-item">
-                            <img src="../../public/svgs/line/heart.svg" alt="like" title="like" draggable="false" />
-                        </li>
-                        <li className="menu-item">
-                            <img src="../../public/svgs/line/eye.svg" alt="see more" title="see more" draggable="false" />
-                        </li>
-                        <li className="menu-item">
-                            <img src="../../public/svgs/line/chat.svg" alt="comments" title="comments" draggable="false" />
-                        </li>
-                        <li className="menu-item">
-                            <img src="../../public/svgs/line/exclamation-circle.svg" alt="report" title="report" draggable="false" />
-                        </li>
-                        <li className="menu-item">
-                            <img src="../../public/svgs/line/bookmark.svg" alt="save" title="save" draggable="false" />
-                        </li>
-                    </ul>
-                    <ul className="tag-list">
-                        <li className="tag" onClick={(e) => window.location='pornhub.com/gay'}>dupa</li>
-                        <li className="tag" onClick={(e) => window.location='pornhub.com/gay'}>kupa</li>
-                    </ul>
-                </div>
-            </div>
+            <Post content={['https://i.pinimg.com/564x/3c/20/5a/3c205a81d5ed0ddab624d6b58b40d91e.jpg']} tags={['alt','teen']} author={{nick:'Zuzaa Olechowska',picture:'https://i.pinimg.com/736x/65/59/37/6559374e52a921ad76785d2175db21c2.jpg',badges:['Verified']}} date={new Date().toString()} likes="6" views={2137} />
+
+            <Post content={['https://thumbs44.redgifs.com/EmotionalFilthyArabianwildcat-mobile.mp4?expires=1679069400&signature=v2:b6f06a502d69b98379bbbf78a6c89742f0c6f20aa36f82b713551e3c7655f46f&for=83.22.63&hash=7011125643']} tags={['ass','bigass','cum','orgasm']} author={{nick:'Użytkownik',picture:'https://i.pinimg.com/564x/3c/20/5a/3c205a81d5ed0ddab624d6b58b40d91e.jpg',badges:['Ban']}} date={new Date().toString()} likes="1501" views={718342} />
+
+            <Post content={['Slub za minute! Zapraszamy na discorda Mokrypapież.sex KOCHAM PIWO JA CHCE WYPIĆ PIWO PIWO TO PALIWO WYRUCHAĆ KURWEKE WSADZIĆ CHUJA GEMBE']} tags={[]} author={{nick:'Zuzaa Olechowska',picture:'https://i.pinimg.com/564x/97/e9/4a/97e94aad4e9550ffde571016105b34ac.jpg',badges:[]}} date={new Date().toString()} likes={1} views={9} />
+
+            <Post content={['https://thumbs44.redgifs.com/AncientMediumvioletredWaterdragons-medium.jpg?expires=1679069400&signature=v2:3763e12bddfd06ace6fbdd4858bc023d56d51669ae555fee7d0a6e330526b767&for=83.22.63.222&hash=6163438793']} tags={['cosplay','bigass','bigtits','hairpussy','emo']} author={{nick:'Agata Max Bednarek',picture:'https://i.pinimg.com/564x/90/02/26/9002260fc9035fbd78fc4c567c6982c1.jpg',badges:['PussyPass']}} date={new Date().toString()} likes="69" views={11491} />
         </div>
     );
 }
 
-function Post({ type, content, tags, author: {nick, picture, bages }, date, likes, views }) { // ! include ban to bages
-    // Content format
+function Post({ content, tags, author, date, likes, views }) { // ! include ban to badges
+    // Content format: check is content a video or a picture or a text
 
     // Tags format
-
-    // Author format
+    const badgesList={
+        "Owner": "../../public/svgs/solid/shield-check.svg",
+        "Admin": "../../public/svgs/line/shield-check.svg",
+        "Mod": "../../public/svgs/line/support.svg",
+        "Developer": "../../public/svgs/line/code.svg",
+        "Verified": "../../public/svgs/line/badge-check.svg",
+        "PussyPass": "../../public/svgs/line/check-circle.svg",
+        "Ban": "../../public/svgs/line/ban.svg",
+    };
 
     // Date format: mm dd, yyyy at hh:mm
     date=new Date(date);
@@ -179,14 +71,26 @@ function Post({ type, content, tags, author: {nick, picture, bages }, date, like
         }
     }
 
+    // const [menuLikeIcon, setMenuLikeIcon] = React.useState(()=>"../../public/svgs/line/heart.svg");
+    // const [liked, setLiked] = React.useState(()=>false);
+
+    // const [menuViewIcon, setMenuViewIcon] = React.useState(()=>"../../public/svgs/line/eye.svg");
+    // const [viewed, setViewed] = React.useState(()=>false);
+
+    // const [menuSaveIcon, setMenuSaveIcon] = React.useState(()=>"../../public/svgs/line/bookmark.svg");
+    // const [saved, setSaved] = React.useState(()=>false);
+
+    // const handleMenuClick = (menuItem) => {};
+
     return (
         <div className="player">
-            <div className="top-bar" onClick={(e) => window.location='pornhub.com/gay'}>
-                <img src="https://i.pinimg.com/736x/65/59/37/6559374e52a921ad76785d2175db21c2.jpg" alt="[profil_name]" title="[profil_name]" className="icon" draggable="false" />
-                <span className="text" title="see profile">Zuzaa Olechowska</span>
+            <div className="top-bar" onClick={(e) => window.location=`search/@${author.nick}`}>
+                <img src={author.picture} alt="" title={author.nick} className="icon" draggable="false" />
+                <span className="text" title="see profile">{author.nick}</span>
                 <div className="badges">
-                    <img src="../../public/svgs/line/badge-check.svg" alt="[badge_name]" title="[badge_name]" className="badge" draggable="false" />
-                    <img src="../../public/svgs/line/badge-check.svg" alt="[badge_name]" title="[badge_name]" className="badge" draggable="false" />
+                    {
+                        author.badges.map((badge, index) => <img src={badgesList[badge]} alt={badge} title={badge} className="badge" draggable="false" />)
+                    }
                 </div>
                 <ul className="info">
                     <li className="info-item">
@@ -200,18 +104,17 @@ function Post({ type, content, tags, author: {nick, picture, bages }, date, like
                 </ul>
             </div>
             <div className="content">
-                <img src="https://i.pinimg.com/564x/3c/20/5a/3c205a81d5ed0ddab624d6b58b40d91e.jpg" alt="" title="" />
+                {
+                    content.map((cont, index) => <img src={cont} alt="" title={author.nick} />)
+                }
             </div>
             <div className="bot-bar">
                 <ul className="menu">
                     <li className="menu-item">
-                        <img src="../../public/svgs/line/heart.svg" alt="like" title="like" draggable="false" />
+                        <img src="../../public/svgs/line/heart.svg" alt="like" title={"likes "+likes} draggable="false" />
                     </li>
                     <li className="menu-item">
                         <img src="../../public/svgs/line/eye.svg" alt="see more" title="see more" draggable="false" />
-                    </li>
-                    <li className="menu-item">
-                        <img src="../../public/svgs/line/chat.svg" alt="comments" title="comments" draggable="false" />
                     </li>
                     <li className="menu-item">
                         <img src="../../public/svgs/line/exclamation-circle.svg" alt="report" title="report" draggable="false" />
@@ -221,8 +124,9 @@ function Post({ type, content, tags, author: {nick, picture, bages }, date, like
                     </li>
                 </ul>
                 <ul className="tag-list">
-                    <li className="tag" onClick={(e) => window.location='pornhub.com/gay'}>dupa</li>
-                    <li className="tag" onClick={(e) => window.location='pornhub.com/gay'}>kupa</li>
+                    {
+                        tags.map((tag, index) => <li className="tag" onClick={(e) => window.location=`search/#${tag}`}>{tag}</li>) // TODO: change to push
+                    }
                 </ul>
             </div>
         </div>
