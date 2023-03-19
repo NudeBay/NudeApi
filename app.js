2d970159c015d0d19bbcdb9da21448565893ed34
Michallesss <mwiemszra@gmail.com>
2023-03-19 16:10:31 +0100

    📦 remove express-device and reset nodemon

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express=require('express');
 const app=express();
-const device=require('express-device');
-app.use(device.capture());
 const dotenv=require('dotenv');
 dotenv.config();
 const path=require('path');
