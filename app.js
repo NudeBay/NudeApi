1fdde5bb9d71c62958df4f0ca71f43585ea43a85
Mikeal <mwiemszra@gmail.com>
2023-03-29 19:40:24 +0200

    🚚 host api and web on separated ports

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
 const express=require('express');
-const app=express();
+const web=express();
+const api=express();
 const dotenv=require('dotenv');
 dotenv.config();
 const path=require('path');
 
-// *Public folder
-app.use(express.json());
-app.use('/public', express.static(path.join(__dirname, '/web/public')));
 
-// *Host Web Server
-const port=process.env.PORT;
-module.exports=app.listen(port, (err)=>{
+
+// ***Host Web Server***
+const webPort=process.env.WEBPORT;
+web.listen(webPort, (err)=>{
     if(err) {
-        return console.error('\x1b[31m','Can not start the host:','\x1b[0m',err);
+        return console.error('\x1b[31m','Can not start the web host:','\x1b[0m',err);
     } else {
-        return console.log('\x1b[32m','Host listening','\x1b[0m',`(on port ${port})...`,'\x1b[0m');
+        return console.log('\x1b[32m','WEB host listening','\x1b[0m',`(on port ${webPort})...`,'\x1b[0m');
     }
 });
 
-// *Import Api Routes
-const apiRoutes=require('./api/routes');
-app.use('/api', apiRoutes);
-// ! set this on subdomain (api.example.com, not example.com/api)
+// Public folder
+web.use(express.json());
+web.use('/public', express.static(path.join(__dirname, '/web/public')));
 
-// *Import Web Routes
+// Import Web Routes
 const webRoutes=require('./web/routes');
-app.use('/', webRoutes);
+web.use('/', webRoutes);
+
+
+
+// ***Host Api Server***
+const apiPort=process.env.APIPORT;
+module.exports=api.listen(apiPort, (err)=>{
+    if(err) {
+        return console.error('\x1b[31m','Can not start the api host:','\x1b[0m',err);
+    } else {
+        return console.log('\x1b[32m','API host listening','\x1b[0m',`(on port ${apiPort})...`,'\x1b[0m');
+    }
+});
+
+// Import Api Routes
+const apiRoutes=require('./api/routes');
+api.use('/', apiRoutes);
