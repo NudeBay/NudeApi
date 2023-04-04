bc1ec4c2927edd4eed3e78f002eb5291a4292d97
Mikeal <mwiemszra@gmail.com>
2023-04-04 21:55:13 +0200

    🦺 beta settings to test

--- api/routes/settings.js
+++ api/routes/settings.js
@@ -16,7 +16,7 @@ router.get('/get', verify, async (req, res) => {
         phone:res.locals.user.phone,
         password:'secret',
         birthDate:res.locals.user.birthDate,
-        bages:res.locals.user.bages, // to show not to change
+        badges:res.locals.user.badges, // to show not to change
         aboutMe:res.locals.user.aboutMe,
         status:res.locals.user.status,
         profilePicture:res.locals.user.profilePicture,
@@ -36,20 +36,20 @@ router.get('/get', verify, async (req, res) => {
 // *Update settings
 router.put('/update', verify, async (req, res) => {
     // Get updated settings
-    const user=new User({
-        nickname:req.body.nickname || undefined,
-        email:req.body.email || undefined,
-        phone:req.body.phone || undefined,
-        password:req.body.password || undefined,
-        birthDate:req.body.birthDate || undefined,
-        aboutMe:req.body.aboutMe || undefined,
-        status:req.body.status || undefined,
-        profilePicture:req.body.profilePicture || undefined,
-        backgroundPicture:req.body.backgroundPicture || undefined,
-        devices:req.body.devices || undefined,
-        settings:req.body.settings || undefined,
+    const user=new User({ // TODO: update to not update all date (only changed) 
+        nickname:req.body.nickname || null,
+        email:req.body.email || null,
+        phone:req.body.phone || null,
+        password:req.body.password || null,
+        birthDate:req.body.birthDate || null,
+        aboutMe:req.body.aboutMe || null,
+        status:req.body.status || null,
+        profilePicture:req.body.profilePicture || null,
+        backgroundPicture:req.body.backgroundPicture || null,
+        devices:req.body.devices || null,
+        settings:req.body.settings || null,
     });
-
+    
     // Validate
     const validateError=await User.validate(user)
     .then(() => null)
@@ -60,25 +60,27 @@ router.put('/update', verify, async (req, res) => {
     });
     if(validateError) return;
     
-    // Check if nickname is taken
-    const [ findError, foundUser ]=await User.findOne({$and: [{nickname: user.nickname}, {"delete.isDeleted":false}]})
+    // Check if nickname or phone is taken
+    const [ findError, foundUser ]=await User.findOne({$and: [{$or: [{nickname: user.nickname}, {email: user.email}, {phone: user.phone}]}, {"delete.isDeleted":false}, {$ne: {_id: res.locals.user._id}}]})
     .then((user) => [ null, user ])
     .catch((err) => [ err, null ]);
     if(findError) return res.status(500).send('500 Internal Server Error');
-    if(foundUser) return res.status(400).send('User with this nickname already exists');
-
-    // Check if email is taken
+    if(foundUser.nickname===user.nickname) return res.status(400).send('Nickname is taken');
+    else if(foundUser.phone===user.phone) return res.status(400).send('Phone is taken');
+    else if(foundUser.email===user.email) return res.status(400).send('Email is taken');
 
     // Confirm by password
+    if(!req.body.oldPassword) return res.status(400).send('Old password is required');
+    const [ confirmError, isMatch ]=await User.comparePassword(req.body.oldPassword, res.locals.user.password)
+    .then((isMatch) => [ null, isMatch ])
+    .catch((err) => [ err, null ]);
+    if(confirmError) return res.status(500).send('500 Internal Server Error');
+    if(!isMatch) return res.status(400).send('Old password is incorrect');
     
-    // Update settings (ignore undefined values)
-    const [ updateError, updatedUser ]=await User.findByIdAndUpdate(res.locals.user._id, user, {$not: undefined, new: true})
+    // Update settings
+    const [ updateError, updatedUser ]=await User.findByIdAndUpdate(res.locals.user._id, {$and: [{$exists: true}, {$set:{user}}]}, {new: true})
     .then((user) => [ null, user ])
-    .catch((err) => {
-        if(err.name==='ValidationError') res.status(400).send(Object.values(err.errors).map(val => val.message));
-        else res.status(500).send('500 Internal Server Error');
-        return [ err, null ];
-    });
-    if(updateError) return;
-    return res.status(200).send('Settings updated');
+    .catch((err) => [ err, null ]);
+    if(updateError) return res.status(500).send('500 Internal Server Error');
+    return res.status(200).send('Deleted');
 });
