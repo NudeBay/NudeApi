310e4d15d099ac1d853ac96434c55f9da78e701f
Michallesss <mwiemszra@gmail.com>
2023-04-19 12:47:40 +0200

    🚧 sign component in progress (fetch error)

--- web/public/react/components/Sign.jsx
+++ web/public/react/components/Sign.jsx
@@ -5,9 +5,24 @@ function Sign(props) {
         document.title='NudeBay | Search';
     }, []);
 
-    const [sign, setSign]=React.useState(() => <In />)
+    const [sign, setSign]=React.useState(() => <In />);
 
-    const apiRequest = (url, method, body) => {};
+    const apiRequest = (event ,url, method, body) => {
+        event.preventDefault();
+
+        fetch(url, { //https://github.com/github/fetch/issues/310   https://www.odoo.com/forum/help-1/it-throws-typeerror-networkerror-when-attempting-to-fetch-resource-216909
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Accept-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify(body)
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+    };
 
     return (
         <div className="sign-container">
@@ -16,16 +31,28 @@ function Sign(props) {
     );
 
     function In() {
+        // Form data
+        const [disabled, setDisabled] = React.useState(() => false);
+        const [buttonContent, setButtonContent] = React.useState(() => 'Sign In');
+
+        // Inputs
+        const [email, setEmail] = React.useState(() => undefined);
+        const [password, setPassword] = React.useState(() => undefined);
+
         return (
-            <form className="form" acceptCharset="UTF-8" autoComplete="on" onSubmit={(e) => e.preventDefault()}>
+            <form className="form" acceptCharset="UTF-8" autoComplete="on" onSubmit={(e) => apiRequest(e, 'localhost:8080/account/login', 'POST', {
+                email: email.toString(),
+                password: password.toString(),
+                device: navigator?.userAgentData?.platform || navigator?.platform || 'unknown',
+            })}>
                 <h1>Sign in</h1>
                 <label htmlFor="email"> Email<sup>*</sup> </label>
-                <input type="email" id="email" placeholder="E-mail" min={5} max={256} autoComplete="email" required />
+                <input type="email" id="email" placeholder="E-mail" minLength={5} maxLength={256} autoComplete="email" disabled={disabled} onInput={(e) => setEmail(e.target.value)} required />
 
                 <label htmlFor="password"> Password<sup>*</sup> </label>
-                <input type="password" id="password" placeholder="Password" min={8} max={128} autoComplete="current-password" required />
+                <input type="password" id="password" placeholder="Password" minLength={8} maxLength={128} autoComplete="current-password" disabled={disabled} onInput={(e) => setPassword(e.target.value)} required />
 
-                <button type="submit"> Sign in </button>
+                <button type="submit"> {buttonContent} </button>
                 <hr />
 
                 <span><a onClick={() => alert('forgot password in progress')}>Forgot password?</a></span>
@@ -35,22 +62,38 @@ function Sign(props) {
     }
 
     function Up() {
+        // Form data
+        const [disabled, setDisabled] = React.useState(() => false);
+        const [buttonContent, setButtonContent] = React.useState(() => 'Sign Up');
+
+        // Inputs
+        const [nickname, setNickname] = React.useState(() => undefined);
+        const [phone, setPhone] = React.useState(() => undefined);
+        const [email, setEmail] = React.useState(() => undefined);
+        const [password, setPassword] = React.useState(() => undefined);
+
         return (
-            <form className="form" acceptCharset="UTF-8" autoComplete="on" onSubmit={(e) => e.preventDefault()}>
+            <form className="form" acceptCharset="UTF-8" autoComplete="on" onSubmit={(e) => apiRequest(e, 'localhost:8080/account/register', 'POST', {
+                nickname: nickname.toString(),
+                phone: phone.toString(),
+                email: email.toString(),
+                password: password.toString(),
+                device: navigator?.userAgentData?.platform || navigator?.platform || 'unknown',
+            })}>
                 <h1>Sign up</h1>
                 <label htmlFor="nickname"> Nickname<sup>*</sup> </label>
-                <input type="text" id="nickname" placeholder="Nickname" min={3} max={32} autoComplete="username" required />
+                <input type="text" id="nickname" placeholder="Nickname" minLength={3} maxLength={32} autoComplete="username" disabled={disabled} onInput={(e) => setNickname(e.target.value)} required />
 
                 <label htmlFor="phone"> Phone </label>
-                <input type="tel" id="phone" placeholder="Phone" min={5} max={32} autoComplete="tel" />
+                <input type="tel" id="phone" placeholder="Phone" minLength={5} maxLength={32} autoComplete="tel" disabled={disabled} onInput={(e) => setPhone(e.target.value)} />
 
                 <label htmlFor="email"> Email<sup>*</sup> </label>
-                <input type="email" id="email" placeholder="E-mail" min={5} max={256} autoComplete="email" required />
+                <input type="email" id="email" placeholder="E-mail" minLength={5} maxLength={256} autoComplete="email" disabled={disabled} onInput={(e) => setEmail(e.target.value)} required />
 
                 <label htmlFor="password"> Password<sup>*</sup> </label>
-                <input type="password" id="password" placeholder="Password" min={8} max={128} autoComplete="new-password" required />
+                <input type="password" id="password" placeholder="Password" minLength={8} maxLength={128} autoComplete="new-password" disabled={disabled} onInput={(e) => setPassword(e.target.value)} required />
 
-                <button type="submit"> Sign in </button>
+                <button type="submit"> {buttonContent} </button>
                 <hr />
 
                 <span>Check out settings and personalize your account.</span>
